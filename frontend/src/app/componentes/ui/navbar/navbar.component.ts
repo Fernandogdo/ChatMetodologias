c6409a39fb41d6e57f5ded202618cda2573f1793
Pedro Salinas <pedrosalinas5@gmail.com>
2019-02-17 20:42:39 -0500

    cambios navbar

--- frontend/src/app/componentes/ui/navbar/navbar.component.ts
+++ frontend/src/app/componentes/ui/navbar/navbar.component.ts
@@ -7,12 +7,15 @@ import { DocenteService } from '../../../servicios/docente/docente.service';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  nombre: string;
   constructor(
     private docenteService: DocenteService,
   ) { }
 
   ngOnInit() {
-
+    let nombre = this.docenteService.obtenerNombresDocente() ;
+    let apellido = this.docenteService.ObtenerApellidosDocente();
+    this.nombre = `${nombre} ${apellido}`;
   }
   cerrarSesion() {
     this.docenteService.cerrarSesionDocente();
